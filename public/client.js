2279af73c3ae0467719af3cef9d858661ef7a194
Terry Thompson <terrylthompsonintx@gmail.com>
2017-11-05 20:00:13 -0600

    Changed buttons that swtich between screens to links

--- public/client.js
+++ public/client.js
@@ -1,14 +1,11 @@
+//STEP 1 - functions and objects definitions
 const yummlyKey = '971c769d4bab882dc3281f0dc6131324';
 const appId = '35372e2c';
 const yummlyEndPoint = 'http://api.yummly.com/v1';
 const yummlyRecipe = 'http://api.yummly.com/v1/api/recipes?'
 var searchTermYummly = 'test';
 
-/*$('#search').hide();
-$('#assign').hide();
-$('#ingredient1').hide();
-$('#ingredient2').hide();
-$('#menu').hide();*/
+
 
 function callYummly(searchTermYummly) {
     var query = yummlyRecipe + '_app_id=' + appId + '&_app_key=' + yummlyKey + '&' + searchTermYummly;
@@ -18,9 +15,10 @@ function callYummly(searchTermYummly) {
 
 //function recipelist();
 
+//STEP 2 - functions and objects usage
+
 $('#continueButton').click(function () {
-    $('#intro').hide();
-    $('#search').show();
+
 })
 
 $('#searchIcon').click(function () {
